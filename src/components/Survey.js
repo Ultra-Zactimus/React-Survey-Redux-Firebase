efde429b2a5232e0950400d8ff6bb930c849550e
Ultra Zacky <ultrazacky@gmail.com>
2022-05-02 14:13:53 -0700

    add dynamic Button Text change for buttons
    Co-authored-by: Daniel Lindsey <dlinds@users.noreply.github.com>
    Co-authored-by: S-Espinet <S-Espinet@users.noreply.github.com>
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import SurveyDetails from './SurveyDetails';
 import { Button } from 'react-bootstrap';
-// import 
 
 const Survey = (props) => {
   const [hidden, setHidden] = useState(false);
+  const buttonText = () => {   
+    if (hidden === false) {
+      return "Take survey!!";
+    } else {
+      return "Cancel";
+    }
+  }
+
   return (
     <React.Fragment>
       <div className="p-5 mx-3 mb-2 border shadow-sm">
         <p>SURVEY NAME: {props.title}</p>
-        <Button variant="primary" onClick={() => setHidden(!hidden)}>Take Survey</Button> 
+        
         {hidden ? <p><SurveyDetails 
             title={props.title}
             q1={props.q1}
@@ -19,6 +26,7 @@ const Survey = (props) => {
             q4={props.q4}
             q5={props.q5}
             id={props.id}/></p> : <p>Details Hidden</p>}
+        <Button variant="primary" onClick={() => setHidden(!hidden)}>{buttonText()}</Button> 
       </div>
       
     </React.Fragment>
