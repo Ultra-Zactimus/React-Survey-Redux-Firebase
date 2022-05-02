fbaef4c61da1713ce39ca1b690695727a30aafe2
Ultra Zacky <ultrazacky@gmail.com>
2022-05-02 10:38:13 -0700

    add firebase to project
    Co-authored-by: Daniel Lindsey <dlinds@users.noreply.github.com>
    Co-authored-by: S-Espinet <S-Espinet@users.noreply.github.com>
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
