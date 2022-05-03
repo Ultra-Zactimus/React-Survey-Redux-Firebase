ad4737a90c5ee4f7962bd8f4d2d6be31a70a5ba5
Ultra Zacky <ultrazacky@gmail.com>
2022-05-03 13:52:25 -0700

    add useNavigate hook to auth files
    Co-authored-by: Daniel Lindsey <dlinds@users.noreply.github.com>
    Co-authored-by: S-Espinet <S-Espinet@users.noreply.github.com>
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,65 +1,58 @@
 import React, { useState } from "react";
 import firebase from "firebase/compat/app";
-import Home from './Home';
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { useNavigate } from 'react-router';
 
 function SignUp() {
 
-  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
   
   function doSignUp(event) {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
       console.log("Successfully Signed Up!");
-      setRedirect(!redirect);
-    }).catch(function(error) {
+      navigate('/');
+    }).catch(function (error) {
       console.log(error.message);
     });
   }
-  if (redirect === false) {
-    return (
-      <React.Fragment>
-        <div className="flex justify-center">
-          <div className="border-2 border-solid border-black max-w-sm rounded overflow-hidden shadow-lg mb-2 mt-4">
-            <h1 className="text-center text-xl">
-              Sign Up
-            </h1>
-            <form 
-              className="bg-white shadow-sm rounded px-8 pt-6 pb-8" 
-              onSubmit={doSignUp}>
-              <input 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
-                type='text'
-                name='email'
-                placeholder='Email' 
-              />
-              <input 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
-                type='password'
-                name='password'
-                placeholder='Password' 
-              />
-              <div className="flex justify-center">
-                <button 
-                  className="bg-warning-500 hover:bg-orange-500 font-bold py-2 px-4 rounded-full" 
-                  type='submit'>
-                    Sign Up
-                </button>
-              </div>
-            </form>
-          </div>
+
+  return (
+    <React.Fragment>
+      <div className="flex justify-center">
+        <div className="border-2 border-solid border-black max-w-sm rounded overflow-hidden shadow-lg mb-2 mt-4">
+          <h1 className="text-center text-xl">
+            Sign Up
+          </h1>
+          <form
+            className="bg-white shadow-sm rounded px-8 pt-6 pb-8"
+            onSubmit={doSignUp}>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
+              type='text'
+              name='email'
+              placeholder='Email'
+            />
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-center leading-tight focus:outline-none focus:shadow-outline mb-2"
+              type='password'
+              name='password'
+              placeholder='Password'
+            />
+            <div className="flex justify-center">
+              <button
+                className="bg-warning-500 hover:bg-orange-500 font-bold py-2 px-4 rounded-full"
+                type='submit'>
+                Sign Up
+              </button>
+            </div>
+          </form>
         </div>
-      </React.Fragment>
-    );  
-  } else {
-    return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    );
-  }
+      </div>
+    </React.Fragment>
+  );
+
 }
 
 export default SignUp;
--- src/components/Signout.js
+++ src/components/Signout.js
@@ -1,42 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import firebase from "firebase/compat/app";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
-import Home from './Home';
+import { useNavigate } from 'react-router';
 
 const SignOut = () => {
 
-
-  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
 
   const doSignOut = (event) => {
     event.preventDefault();
     firebase.auth().signOut()
       .then(function () {
         console.log("Successfully signed out!");
-        setRedirect(!redirect);
+        navigate('/');
       }).catch(function (error) {
         console.log(error.message);
       });
   }
-  if (redirect === false) {
-    return (
-      <React.Fragment>
-        <h1 className="text-center text-xl mb-2">
-          Sign Out
-        </h1>
-        <button className="bg-yellow-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full"
-          onClick={doSignOut}>
-          Sign Out
-        </button>
-      </React.Fragment>
-    );
-  } else {
-    return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    );
-  }
+
+  return (
+    <React.Fragment>
+      <h1 className="text-center text-xl mb-2">
+        Sign Out
+      </h1>
+      <button className="bg-yellow-500 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full"
+        onClick={doSignOut}>
+        Sign Out
+      </button>
+    </React.Fragment>
+  );
 }
 
 export default SignOut;
